bdc089935748ff6c934b7e9c147c4fa0cbc23948
Chimegerdene Drl <Lenovo@DESKTOP-DG7QGI5.(none)>
2025-05-07 11:18:58 +0800

    done

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -31,8 +31,43 @@ export const createUser = async (req, res) => {
 
 export const getUsers = async (req, res) => {
   try {
-    const users = await UserModel.find();
+    const users = await UserModel.find().select("-password");
     res.status(200).send({ success: true, users: users }).end();
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ success: false, error: error }).end();
+  }
+};
+
+export const getUser = async (req, res) => {
+  const { _id } = req.params;
+  try {
+    const user = await UserModel.findById(_id);
+    res.status(200).send({ success: true, user: user }).end();
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ success: false, error: error }).end();
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { _id } = req.params; // Get userId from request params or body
+  const { username, email, phoneNumber, password } = req.body;
+
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      _id, // Find user by ID
+      { username, email, phoneNumber, password }, // The data to update
+      { new: true, runValidators: true } // Ensure the new document is returned and validators are applied
+    );
+
+    if (!updatedUser) {
+      return res
+        .status(404)
+        .send({ success: false, message: "User not found" });
+    }
+
+    res.status(200).send({ success: true, user: updatedUser });
   } catch (error) {
     console.log(error);
     res.status(400).send({ success: false, error: error }).end();
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,15 @@
 import express from "express";
-import { createUser, getUsers } from "../controller/user.js";
+import {
+  createUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controller/user.js";
 
 export const userRouter = express.Router();
 
-userRouter.post("/", createUser).get("/", getUsers);
+userRouter
+  .post("/", createUser)
+  .get("/", getUsers)
+  .put("/:_id", updateUser)
+  .get("/:_id", getUser);
