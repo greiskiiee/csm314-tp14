07a6197a09dc619e2593a038c7d735aeb9de6b9d
Chimegerdene Drl <Lenovo@DESKTOP-DG7QGI5.(none)>
2025-05-10 15:58:12 +0800

    home page responsive done

--- front/src/app/home/page.js
+++ front/src/app/home/page.js
@@ -2,17 +2,14 @@
 import {
   NavigationMenu,
   NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
@@ -27,7 +24,6 @@ import {
   DialogFooter,
   DialogClose,
 } from "@/components/ui/dialog";
-
 import { History, ReceiptText, User, UserRoundPen } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
@@ -67,19 +63,23 @@ export default function Home() {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center bg-[#2a2c41] ">
       <div className="w-[90%] flex justify-between mb-3 items-center">
-        <div className="w-fit h-full text-[#ff724c] montserrat font-[500] text-[26px] flex justify-center items-center py-2 box-border rounded-md">
+        <div className="w-fit h-full text-[#ff724c] montserrat font-[500] text-[18px] lg:text-[26px] flex justify-center items-center py-2 box-border rounded-md">
           EatWell+
         </div>
         <NavigationMenu>
-          <NavigationMenuList className="gap-2">
+          <NavigationMenuList className="gap-2 ">
             <NavigationMenuItem>
-              <NavigationMenuTrigger>Recipes</NavigationMenuTrigger>
+              <NavigationMenuTrigger className="invisible lg:visible">
+                Recipes
+              </NavigationMenuTrigger>
               <NavigationMenuContent>
                 <NavigationMenuLink>Link</NavigationMenuLink>
               </NavigationMenuContent>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuTrigger>Advices</NavigationMenuTrigger>
+              <NavigationMenuTrigger className="invisible lg:visible">
+                Advices
+              </NavigationMenuTrigger>
               <NavigationMenuContent>
                 <NavigationMenuLink>Link</NavigationMenuLink>
               </NavigationMenuContent>
@@ -87,10 +87,13 @@ export default function Home() {
           </NavigationMenuList>
         </NavigationMenu>
 
-        <div className="bg-[#ff724c] flex justify-center items-center w-[40px] h-[40px] rounded-full">
+        <div className="w-[24px] h-[24px] bg-[#ff724c] flex justify-center items-center lg:w-[40px] lg:h-[40px] rounded-full">
           <Sheet>
             <SheetTrigger className="flex justify-center items-center">
-              <User color="white" size={24} />
+              <User
+                color="white"
+                className="w-[16px] h-[16px] lg:w-[24px] lg:h-[24px]"
+              />
             </SheetTrigger>
             <SheetContent>
               <SheetHeader className="h-full flex flex-col justify-between items-start">
@@ -167,13 +170,13 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="w-[90%] h-[90%] flex justify-center items-center rounded-xl  ">
-        <div className="w-1/2 h-full bg-[#f4f3f8] rounded-l-xl flex flex-coljustify-center items-center py-8 px-6 gap-6">
+      <div className="w-[90%] h-[90%] flex flex-col lg:flex-row justify-center items-center rounded-xl  ">
+        <div className="w-full h-1/2 lg:w-1/2 lg:h-full bg-[#f4f3f8] rounded-t-xl lg:rounded-l-xl lg:rounded-t-none flex flex-coljustify-center items-center py-8 px-6 gap-6">
           <div className="w-full h-full flex justify-center items-center border border-gray-300 rounded-md">
             CONTENT
           </div>
         </div>
-        <div className="w-1/2 h-full flex flex-col justify-between gap-2 py-8 px-6 bg-[#f4f3f8] rounded-r-xl">
+        <div className="w-full h-1/2 lg:w-1/2 lg:h-full flex flex-col justify-between gap-2 py-8 px-6 bg-[#f4f3f8] rounded-b-xl lg:rounded-b-none lg:rounded-r-xl">
           <div className="w-full h-1/2 flex justify-center items-center bg-[#7c7c7b]/50 rounded-md">
             CONTENT
           </div>
